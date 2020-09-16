ad7e1eb1d18ccf29221c1ae4724116e3649d8214
Sriram Thiagarajan <sri.sjc@gmail.com>
2020-09-16 08:02:45 +0530

    optionally pass headers to fetcher
    GitHub has a limit of 60 requests per hour if you're unauthenticated. If we do static generation at build time for all followers, this limit will easily be reached. So adding the ability to pass auth headers. Uusing a personal access token for static generation gives 5000 requests per hour.

--- src/api.js
+++ src/api.js
@@ -1,10 +1,14 @@
-const fetcher = (url) => fetch(url).then((res) => res.json());
+const fetcher = (url, headers) =>
+  fetch(url, {
+    headers
+  }).then((res) => res.json());
 
 const user = process.env.NEXT_PUBLIC_GH_USER;
-export async function getFollowers() {
-  return fetcher(`https://api.github.com/users/${user}/followers`);
+
+export async function getFollowers(headers) {
+  return fetcher(`https://api.github.com/users/${user}/followers`, headers);
 }
 
-export async function getUserDetails(login) {
-  return fetcher(`https://api.github.com/users/${login}`);
+export async function getUserDetails(login, headers) {
+  return fetcher(`https://api.github.com/users/${login}`, headers);
 }
